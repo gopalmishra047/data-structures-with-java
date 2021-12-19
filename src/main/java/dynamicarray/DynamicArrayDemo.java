3014995b356889edb9add49ec2c2935f8e457d66
gopalmishra047 <42767726+gopalmishra047@users.noreply.github.com>
2021-12-19 13:42:45 +0530

    implement dynamic array

---
+++ src/main/java/dynamicarray/DynamicArrayDemo.java
@@ -0,0 +1,127 @@
+package dynamicarray;
+
+import java.util.Iterator;
+
+@SuppressWarnings("unchecked")
+class Array<T> implements Iterable<T> {
+
+  private T[] array;
+  private int length;
+  private int capacity;
+
+  public Array(int capacity) {
+    if (capacity < 0) {
+      throw new IllegalArgumentException("Capacity should be greater than 0");
+    }
+    this.capacity = capacity;
+    array = (T[]) new Object[capacity];
+  }
+
+  public void set(int index, T element) {
+    array[index] = element;
+  }
+
+  public T get(int index) {
+    return array[index];
+  }
+
+  public void add(T element) {
+    if (length + 1 >= capacity) { // Check if new length would exceed current capacity
+      if (capacity == 0) {
+        capacity = 1;
+      } else {
+        capacity = capacity * 2; // double the size
+      }
+      T[] newArray = (T[]) new Object[capacity]; // create a new array with new size
+      for (int i = 0; i < length; i++) {
+        newArray[i] = array[i]; // copy the old array into new one
+      }
+      array = newArray; // set the new array as the current array
+    }
+    array[length++] = element; // add the new element into array
+  }
+
+  public boolean remove(T element) {
+    int index = indexOf(element);
+    removeAt(index);
+    return true;
+  }
+
+  public int indexOf(T element) {
+    for (int i = 0; i < length; i++) {
+      if (element == null) {
+        if (array[i] == null) {
+          return i;
+        }
+      } else {
+        if (element.equals(array[i])) {
+          return i;
+        }
+      }
+    }
+    return -1;
+  }
+
+  public T removeAt(int index) {
+    if (index < 0 || index >= length) {
+      throw new ArrayIndexOutOfBoundsException("Given index is oud of bound.");
+    }
+    T element = array[index];
+    T[] newArray = (T[]) new Object[length - 1];
+    for (int i = 0, j = 0; i < length; i++, j++) {
+      if (i == index) {
+        j--; // skip over index by fixing j temporarily
+      } else {
+        newArray[j] = array[i];
+      }
+    }
+    array = newArray;
+    capacity = --length;
+    return element;
+  }
+
+  public void clear() {
+    for (int i = 0; i < length; i++) {
+      array[i] = null;
+    }
+    length = 0;
+  }
+
+  public int size() {
+    return length;
+  }
+
+  public boolean isEmpty() {
+    return length == 0;
+  }
+
+  @Override
+  public Iterator<T> iterator() {
+    return new java.util.Iterator<>() {
+      int index = 0;
+
+      @Override
+      public boolean hasNext() {
+        return index < length;
+      }
+
+      @Override
+      public T next() {
+        return array[index++];
+      }
+
+      @Override
+      public void remove() {
+        throw new UnsupportedOperationException();
+      }
+    };
+  }
+
+  public String toString() {
+    StringBuilder builder = new StringBuilder("[");
+    for (int i = 0; i < length - 1; i++) {
+      builder.append(array[i]).append(",");
+    }
+    return builder.append(array[length - 1]).append("]").toString();
+  }
+}
---
+++ src/test/java/dynamicarray/DynamicArrayTest.java
@@ -0,0 +1,103 @@
+package dynamicarray;
+
+import java.util.Iterator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
+class DynamicArrayTest {
+
+  private final Array<Integer> array = new Array<>(5);
+
+  @BeforeEach
+  void setUp() {
+    array.add(2);
+    array.add(4);
+    array.add(0);
+    array.add(9);
+    array.add(7);
+  }
+
+  @AfterEach
+  void tearDown() {
+    array.clear();
+  }
+
+  @Test
+  void set() {
+    array.set(2, 11);
+    assertThat(array.get(2)).isEqualTo(11);
+  }
+
+  @Test
+  void get() {
+    assertThat(array.get(4)).isEqualTo(7);
+    assertThat(array.get(array.size() + 1)).isNull();
+  }
+
+  @Test
+  void add() {
+    assertThat(array.size()).isEqualTo(5);
+    array.add(10);
+    assertThat(array.get(5)).isEqualTo(10);
+    assertThat(array.size()).isEqualTo(6);
+  }
+
+  @Test
+  void remove() {
+    assertThat(array.remove(9)).isTrue();
+    assertThat(array.size()).isEqualTo(4);
+    assertThat(array.indexOf(9)).isEqualTo(-1);
+    assertThat(array.indexOf(7)).isEqualTo(3);
+  }
+
+  @Test
+  void indexOf() {
+    assertThat(array.indexOf(9)).isEqualTo(3);
+    assertThat(array.indexOf(20)).isEqualTo(-1);
+  }
+
+  @Test
+  void removeAt() {
+    assertThat(array.removeAt(1)).isEqualTo(4);
+    assertThat(array.size()).isEqualTo(4);
+    assertThatExceptionOfType(ArrayIndexOutOfBoundsException.class)
+        .isThrownBy(() -> array.removeAt(10))
+        .withMessage("Given index is oud of bound.");
+  }
+
+  @Test
+  void clear() {
+    array.clear();
+    assertThat(array.size()).isEqualTo(0);
+  }
+
+  @Test
+  void size() {
+    assertThat(array.size()).isEqualTo(5);
+  }
+
+  @Test
+  void isEmpty() {
+    assertThat(array.isEmpty()).isFalse();
+    array.clear();
+    assertThat(array.isEmpty()).isTrue();
+  }
+
+  @Test
+  void iterator() {
+    Iterator<Integer> iterator = array.iterator();
+    for (int i = 0; i < array.size(); i++) {
+      assertThat(iterator.hasNext()).isTrue();
+      iterator.next();
+    }
+    assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(iterator::remove);
+  }
+
+  @Test
+  void testToString() {
+    assertThat(array.toString()).isEqualTo("[2,4,0,9,7]");
+  }
+}
